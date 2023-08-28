79b1a0fda1652fc7330845e28752a93fb33d9775
Henrique Cosmo <124743713+HenriqueCosmo@users.noreply.github.com>
2023-08-27 22:57:55 -0300

    arrumando design e criando função de click

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-const key = 'e19949b2f5bbf124c9efaf5b77b31e6e';
-let city = 'londres';
+
+const formBtn = document.querySelector("#envioNome")
 
 
-let url = `https://api.openweathermap.org/data/2.5/weather?q={${city}}&appid={${key}`;
 
-fetch (url)
-.then((response) => response.json())
-.then((data) => console.log(data));
+async function carregarDadosCidade(cidade){
+    const key = 'e19949b2f5bbf124c9efaf5b77b31e6e';
+    const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+}
+
+
+formBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const cidade = document.querySelector(".cidade").value;
+
+    carregarDadosCidade(cidade);
+    
+});
+
+
+
+
+
+
+
