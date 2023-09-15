d24ec2c3dc7be2f6ad99bd04b748c5c496fa62d4
Henrique Cosmo <124743713+HenriqueCosmo@users.noreply.github.com>
2023-09-15 10:38:22 -0300

    finalizado
    resgate dos valores da api e configurações finais da aplicação

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 
 const formBtn = document.querySelector("#envioNome")
+const key = 'e19949b2f5bbf124c9efaf5b77b31e6e';
 
 
 
 async function carregarDadosCidade(cidade){
-    const key = 'e19949b2f5bbf124c9efaf5b77b31e6e';
-    const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`)
+    const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    
+    colocarDadosNaTela(dados)
 
+    
+}
+
+function colocarDadosNaTela(dados){
+    console.log(dados)
+    document.querySelector(".textDetail").innerHTML = `Tempo em ${dados.name}`;
+    document.querySelector(".temp").innerHTML = `${Math.floor(dados.main.temp)}°C`
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = `Umidade:  ${dados.main.humidity}%`
+    document.querySelector(".icone").src = ` https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
 
